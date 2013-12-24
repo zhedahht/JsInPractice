90f15a4ae6caf436ab13833a165341bb7b0f361c
zhedahht <zhedahht@hotmail.com>
2013-12-24 13:34:01 +0800

    Add a Range Constructor

--- MerryChristmas/Snow.js
+++ MerryChristmas/Snow.js
@@ -32,17 +32,25 @@ function initImage(imagePath) {
     return image;
 }
 
+function Range(min, max) {
+	this.min = min;
+	this.max = max;
+}
+
 function initSnow(width, height) {
-    var minRasius = 3,
-        maxRadius = 10,
-        minSpeedY = 1,
-        maxSpeedY = 3,
+    var radiusRange = new Range(3, 10),
+		speedYRange = new Range(1, 3),
         speedX = 0.05,
-        minAlpha = 0.5,
-        maxAlpha = 1.0,
-        minMoveX = 4,
-        maxMoveX = 18;
-    var snowSettings = new SnowSettings(minRasius, maxRadius, width, height, minSpeedY, maxSpeedY, speedX, minAlpha, maxAlpha, minMoveX, maxMoveX);
+		alphaRange = new Range(0.5, 1.0),
+		moveXRange = new Range(4, 18);
+		
+    var snowSettings = new SnowSettings(radiusRange, 
+								width, 
+								height, 
+								speedYRange, 
+								speedX, 
+								alphaRange, 
+								moveXRange);
     
     var snow = [];
     var snowNumber = 200;
@@ -54,10 +62,8 @@ function initSnow(width, height) {
 }
 
 ChristmasSnow.prototype.render = function() {
-    // render background image
     this.canvasContext.drawImage(this.image, 0, 0);
     
-    // render snow
     for(var i = 0; i < this.snow.length; ++i) {
         this.snow[i].render(this.canvasContext);
     }
@@ -69,32 +75,28 @@ ChristmasSnow.prototype.update = function() {
     }
 }
 
-function SnowSettings(minRadius, maxRadius, maxX, maxY, minSpeedY, maxSpeedY, speedX, minAlpha, maxAlpha, minMoveX, maxMoveX) {
-    this.minRadius = minRadius;
-    this.maxRadius = maxRadius;
+function SnowSettings(radiusRange, maxX, maxY, speedYRange, speedX, alphaRange, moveXRange) {
+    this.radiusRange = radiusRange;
     this.maxX = maxX;
     this.maxY = maxY;
+    this.speedYRange = speedYRange;
     this.speedX = speedX;
-    this.minSpeedY = minSpeedY;
-    this.maxSpeedY = maxSpeedY;
-    this.minAlpha = minAlpha;
-    this.maxAlpha = maxAlpha;
-    this.minMoveX = minMoveX;
-    this.maxMoveX = maxMoveX;
+    this.alphaRange = alphaRange;
+    this.moveXRange = moveXRange;
 }
 
 function Snow(snowSettings) {
     this.snowSettings = snowSettings;
     
-    this.radius = randomInRange(snowSettings.minRadius, snowSettings.maxRadius);
+    this.radius = randomInRange(snowSettings.radiusRange);
     this.initialX = Math.random() * snowSettings.maxX;
     this.y = Math.random() * snowSettings.maxY;
-    this.speedY = randomInRange(snowSettings.minSpeedY, snowSettings.maxSpeedY);
+    this.speedY = randomInRange(snowSettings.speedYRange);
     this.speedX = snowSettings.speedX;
-    this.alpha = randomInRange(snowSettings.minAlpha, snowSettings.maxAlpha);
+    this.alpha = randomInRange(snowSettings.alphaRange);
     this.angle = Math.random(Math.PI * 2);
     this.x = this.initialX + Math.sin(this.angle);
-    this.moveX = randomInRange(snowSettings.minMoveX, snowSettings.maxMoveX);
+    this.moveX = randomInRange(snowSettings.moveXRange);
 }
 
 Snow.prototype.render = function(canvasContext) {
@@ -119,7 +121,7 @@ Snow.prototype.update = function() {
     this.x = this.initialX + this.moveX * Math.sin(this.angle);
 }
 
-function randomInRange(min, max) {
-    var random = Math.random() * (max - min) + min;
+function randomInRange(range) {
+    var random = Math.random() * (range.max - range.min) + range.min;
     return random;
 }
